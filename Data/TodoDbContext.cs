ab8dfe45e31cacff1fdacd372d933eabd2223f4c
romatvX_ <asssfsa010@gmail.com>
2026-03-10 22:28:42 +0200

    Refactor TodoDbContext to simplify entity configurations and update DbSet properties

--- Data/TodoDbContext.cs
+++ Data/TodoDbContext.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using ToDo_Project.Models.Domain.Entities;
+using Models.Domain.Entities;
 
 namespace ToDo_Project.Data;
 
@@ -8,58 +8,17 @@ public class TodoDbContext : DbContext
     public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
     {
     }
-    
-    // DbSets for entities
-    public DbSet<Task> Tasks { get; set; } = null!;
-    public DbSet<OneTimeTask> OneTimeTasks { get; set; } = null!;
-    public DbSet<RepeatingTask> RepeatingTasks { get; set; } = null!;
-    public DbSet<Reminder> Reminders { get; set; } = null!;
-    public DbSet<TaskHistory> TaskHistories { get; set; } = null!;
-    
+    public DbSet<TaskItem> Tasks { get; set; }
+    public DbSet<Reminder> Reminders { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        
-        // Task entity configuration
-        modelBuilder.Entity<Task>()
-            .HasKey(t => t.Id);
-            
-        modelBuilder.Entity<Task>()
-            .Property(t => t.Title)
-            .IsRequired()
-            .HasMaxLength(200);
-        
-        modelBuilder.Entity<Task>()
-            .HasDiscriminator<string>("TaskType")
-            .HasValue<OneTimeTask>("OneTimeTask")
-            .HasValue<RepeatingTask>("RepeatingTask");
-        
-        // OneTimeTask configuration
-        modelBuilder.Entity<OneTimeTask>()
-            .ToTable("OneTimeTasks");
-        
-        // RepeatingTask configuration
-        modelBuilder.Entity<RepeatingTask>()
-            .ToTable("RepeatingTasks");
-        
-        // Reminder entity configuration
-        modelBuilder.Entity<Reminder>()
-            .HasKey(r => r.Id);
-        
-        modelBuilder.Entity<Reminder>()
-            .HasOne(r => r.Task)
-            .WithMany()
+
+        modelBuilder.Entity<TaskItem>()
+            .HasMany(t => t.Reminders)
+            .WithOne(r => r.TaskItem)
             .HasForeignKey(r => r.TaskId)
-            .OnDelete(DeleteBehavior.Cascade);
-        
-        // TaskHistory entity configuration
-        modelBuilder.Entity<TaskHistory>()
-            .HasKey(th => th.Id);
-        
-        modelBuilder.Entity<TaskHistory>()
-            .HasOne(th => th.Task)
-            .WithMany()
-            .HasForeignKey(th => th.TaskId)
             .OnDelete(DeleteBehavior.Cascade);
     }
 }
